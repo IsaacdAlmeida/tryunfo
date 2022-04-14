db7e1ed7149845d9f304e40d1fc2490cc6471b55
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-04-14 18:12:11 -0300

    fix: fix lint adding typechecking

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
-      cardImage, cardRare, cardTrunfo, hasTrunfo,
+      cardImage, cardRare, cardTrunfo, /* hasTrunfo */
       isSaveButtonDisabled, onInputChange, onSaveButtonClick } = this.props;
 
     return (
@@ -113,5 +114,20 @@ class Form extends React.Component {
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.number.isRequired,
+  cardAttr2: PropTypes.number.isRequired,
+  cardAttr3: PropTypes.number.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  // hasTrunfo: PropTypes.bool.isRequired, // ainda não utilizei, mas acredito que sjea booleano
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
